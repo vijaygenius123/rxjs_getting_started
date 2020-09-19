01a22d33112410749ef4f9414e0b65aac9519a2e
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-19 19:37:27 +0530

    Producing values asynchronously

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
-import { from, Observer, Observable } from "rxjs";
+import {  Observable } from "rxjs";
 
 let numbers = [1, 2, 3, 4, 5];
 let source = Observable.create(function(observer) {
 
-    for (let n of numbers)(
-        observer.next(n)
-    );
+    let index = 0;
 
-    observer.complete();
+    let produceValue = () => {
+        observer.next(numbers[index++]);
+
+        if(index < numbers.length){
+            setTimeout(()=> {
+                produceValue()
+            }, 1500);
+        }else {
+            observer.complete();
+        }
+    }
+
+    produceValue();
+
 });
 
 source.subscribe(
     value => {console.log(`Value: ${value}`)},
-    error => {console.log(`Error: ${error}`)},
-    () => {console.log('Complete')}
-    );
+    error => {console.log('Error: ${error}')},
+    () => console.log('Complete')
+);
