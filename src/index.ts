a620fccca9c969c09348b5aec65009f6125ec0f6
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-27 17:35:53 +0530

    Getting Data From mousemove

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,17 @@
-import {  Observable, pipe } from "rxjs";
-import {filter, map} from 'rxjs/operators';
+import {fromEvent} from "rxjs";
+import {map} from "rxjs/operators";
 
-let numbers = [1, 2, 3, 4, 5];
-let source = Observable.create(function(observer) {
 
-    let index = 0;
+let source: any;
+source = fromEvent(document, 'mousemove')
+    .pipe(
+        map((e: MouseEvent) => {
+            return {
+                x: e.clientX,
+                y: e.clientY
+            }
+        })
+    )
 
-    let produceValue = () => {
-        observer.next(numbers[index++]);
 
-        if(index < numbers.length){
-            setTimeout(()=> {
-                produceValue()
-            }, 1500);
-        }else {
-            observer.complete();
-        }
-    }
-
-    produceValue();
-
-}).pipe(
-    map(function (n:number){
-        return n * 10;
-    }),
-    filter(function (n:number){
-        return n < 25
-    })
-)
-
-source.subscribe(
-    value => {console.log(`Value: ${value}`)},
-    error => {console.log('Error: ${error}')},
-    () => console.log('Complete')
-);
+source.subscribe(value => console.log(value));
